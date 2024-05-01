d4cb65bac90cceeb483fa62ee228875a284dae21
takunda hwengwere <munashwengz@gmail.com>
2024-05-01 15:41:22 +0200

    initialisation

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
 import tutrle
 
+
+def pattern(size, levels, angle):
+    if levels - -0:
+        return
+    forward(size)
+    right(angle)
+    tree(size * 0.8, levels - 1, angle)
+    left(angle * 2)
+    tree(size * 0.8, levels - 1, angle)
