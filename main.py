bcec16d868196cfe34c2d27371bd7b8547b309bd
takunda hwengwere <munashwengz@gmail.com>
2024-05-01 20:29:30 +0200

    more shapes

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from turtle import *
+import random
 
 shape("turtle")
-speed(5)
+speed(0)
 width(10)
 bgcolor("orange")
+goto(0,0)
 # color("orange")
 def pattern(size, levels, angle):
     if levels ==0:
@@ -65,8 +67,43 @@ def snowflake_side(length, levels):
     snowflake_side(length, levels - 1)
 
 
+def create_snowflake(sides, length):
+    # normal snowflake
+    for _ in range(sides):
+        snowflake_side(length, sides)
+        right(360 / sides)
 
+
+def create_weirdcircle(sides, size, angle, sides_multiplier, turn_angle):
+    penup()
+    goto(random.randint(-200, 200), random.randint(-200, 200))
+    pendown()
+    for _ in range(sides * sides_multiplier):
+        snowflake_side(size, sides)
+        right(angle / sides)
+    penup()
+    right(turn_angle)
+    # forward(size * 2)
+    pendown()
 # line_with_humps(100, 3, 30)
 
-snowflake_side(200, 2)
+def weird_circle(sides, size, angle, sides_multiplier, turn_angle, loop_range):
+    for i in range(loop_range):
+        create_weirdcircle(sides, size, angle, sides_multiplier, turn_angle)
+        print(i)
+
+# spider circle
+# weird_circle(3, 100, 200, 1, 90,35)
+weird_circle(3,135,338,1,261,57)
+
+size = random.randint(100,200)
+angle = random.randint(1,360)
+sides_multiplier = random.randint(1,10)
+turn_angle = random.randint(200,400)
+loop_range = random.randint(1,100)
+
+print(f"size: {size} angle: {angle} sizes_multiplier: {sides_multiplier} turn_angle: {turn_angle} loop_range: {loop_range}")
+
+# weird_circle(3, size, angle, sides_multiplier, turn_angle, loop_range)
+
 mainloop()
