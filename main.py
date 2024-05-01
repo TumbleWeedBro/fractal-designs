bb4dd146c8c1d08906c3f6cb9288c04747a6f681
takunda hwengwere <munashwengz@gmail.com>
2024-05-01 15:45:56 +0200

    first design

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
-import tutrle
+from turtle import *
+
+shape("turtle")
+speed(0)
 
 
 def pattern(size, levels, angle):
-    if levels - -0:
+    if levels ==0:
         return
     forward(size)
     right(angle)
-    tree(size * 0.8, levels - 1, angle)
+
+    pattern(size * 0.8, levels - 1, angle)
+
     left(angle * 2)
-    tree(size * 0.8, levels - 1, angle)
+
+    pattern(size * 0.8, levels - 1, angle)
+
+    right(angle)
+    backward(size)
+
+
+pattern(200, 10, 60)
+mainloop()
