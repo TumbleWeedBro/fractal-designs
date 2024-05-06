231421dc3b21aace63eac30ef91528afab2f5f9a
ndivhuwotheunknown <166398227+ndivhuwotheunknown@users.noreply.github.com>
2024-05-06 22:15:44 +0200

    .

--- main.py
+++ main.py
@@ -9,28 +9,30 @@ y_axis = 120
 # circle(100)
 
 # circle(100 / 2, 180)
-x_coord = -800
-y_coord = -400
+#x_coord = -800 OG
+x_coord =-650
+#y_coord = -400 OG
+y_coord = -250
 speed(0)
 print(x_coord, y_coord)
-radius_big = 100
+radius_big = 110
 radius_small = 30
 penup()
 goto(x_coord, y_coord)
 pendown()
-for n in range(5):
+for n in range(3):
     penup()
     goto(x_coord, y_coord)
     pendown()
-    for i in range(5):
+    for i in range(6):
         goto(x_coord, y_coord)
         circle(radius_big / 2, 180)
         circle(radius_big)
         goto(x_coord, y_coord)
         circle(radius_small / 2, 180)
         circle(radius_small)
-        x_coord += 400
-    y_coord += 200
+        #x_coord += 250
+    y_coord += 250
     x_coord = -800
 
 
